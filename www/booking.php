07627d73ce887353a447f0cc4874e1ee11154748
Cameron Stewart <cameron@mail.shptech.com>
2010-05-01 18:35:41 +1000

    booking added

--- www/booking.php
+++ www/booking.php
@@ -1,12 +1,25 @@
-<?php
+
+<?php require_once("inc/header.php"); ?>
+
+Database info: <br>
+<pre><?php
 if (isset($_POST["name"])) { 
      $name = $_POST["name"];
-     echo "Hey got your booking $name"; 
+     echo "Hey got your booking $name\n";
+     $namearr = split(" ", $name);
+     $firstname = $namearr[0];
+     $lastname = $namearr[1];
+     $email = $_POST["email"];
+     $phone = $_POST["phone"];
+     $seats = $_POST["seats"];
 
+     $sql = "INSERT INTO `booking`.`booking` (`id`, `first_name`, `last_name`, `email`, `phone`, `seats`, `arrival`, `no_show`) VALUES (NULL, '$firstname', '$lastname', '$email', '$phone', '$seats', '2010-05-17 18:10:54', '0');";
+     echo "SQL: $sql\n";
+     mysql_query($sql, $link);
 }
 
 ?>
-
+</pre>
 <form action='' method="post">
 <input type="text" name="name" /><br />
 
@@ -15,3 +28,4 @@ if (isset($_POST["name"])) {
 <input type="text" name="seats" /><br />
 <input type="submit" value="Book" />
 </form>
+<?php require_once("inc/footer.php"); ?>
