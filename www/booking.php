95e1a3f2643488d6127a21bc8efb8e31a14d85d7
unknown <Nelson@.(none)>
2010-05-01 18:43:35 +1000

    added css and inc

--- www/booking.php
+++ www/booking.php
@@ -8,9 +8,12 @@ if (isset($_POST["name"])) {
 ?>
 
 <form action='' method="post">
-<input type="text" name="name" /><br />
-
-<input type="text" name="email" /><br />
+    <label for="first_name">First Name:</label>
+        <input type="text" name="first_name" /><br />
+    <label for="last_name">Last Name:</label>
+        <input type="text" name="email" /><br />
+    <label for="email">Email:</label>
+        <input type="text" name="email" /><br />
 <input type="text" name="phone" /><br />
 <input type="text" name="seats" /><br />
 <input type="submit" value="Book" />
